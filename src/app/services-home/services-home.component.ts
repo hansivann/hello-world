b5de0e9dc8c868f96aadb74053b5c7272597ecc3
Ivan Francisco <hansivann23@gmail.com>
2025-03-04 08:47:33 -0800

    learnded debugging in Angular

--- src/app/services-home/services-home.component.ts
+++ src/app/services-home/services-home.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { HighlightDirective } from './highlight.directive';
 
 @Component({
   selector: 'app-services-home',
-  imports: [],
+  standalone: true,
+  imports: [HighlightDirective],
   templateUrl: './services-home.component.html',
   styleUrl: './services-home.component.css'
 })
