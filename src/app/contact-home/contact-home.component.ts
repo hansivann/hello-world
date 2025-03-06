c312f4c9c7cbf5ba78cede484e56add60edeafce
Ivan Francisco <hansivann23@gmail.com>
2025-03-06 01:09:26 -0800

    Implement form functionality and dynamic data binding in contact and projects components

--- src/app/contact-home/contact-home.component.ts
+++ src/app/contact-home/contact-home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-contact-home',
@@ -6,6 +6,14 @@ import { Component } from '@angular/core';
   templateUrl: './contact-home.component.html',
   styleUrl: './contact-home.component.css'
 })
-export class ContactHomeComponent {
+export class ContactHomeComponent implements OnInit {
+  isUnchanged = true;
+  onCancel(event?: KeyboardEvent) {
+    const evtMsg = event ? ' Event target is ' + (event.target as HTMLElement).textContent : '';
+    alert('Cancelled.' + evtMsg);
+  }
 
+  constructor() {}
+
+  ngOnInit(): void {}
 }
